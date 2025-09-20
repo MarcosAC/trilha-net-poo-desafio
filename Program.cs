22b2e3613dd15b78c0fe5fc0caff3fd083d1b05d
Marcos Aurelio <marcos.aucorrea@gmail.com>
2025-09-19 23:17:32 -0300

    Desafio POO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main()
+    {
+        Nokia nokia = new Nokia("123456789", "Nokia 3310", "111111111", 16);
+        nokia.Ligar();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        Console.WriteLine();
+
+        Iphone iphone = new Iphone("987654321", "iPhone 15", "222222222", 128);
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
